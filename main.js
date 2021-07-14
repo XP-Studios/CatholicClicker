efe03681cf983ea2808d6f0a480c8351cc2d5112
XP-Studios <66804726+XP-Studios@users.noreply.github.com>
2021-07-14 19:47:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-var diaconalAchieved = 0;  //achievement variables
-var ordinationAchieved = 0;
-
 var clickPower = 1; //liras per click
 
 var liras = 0;
-function liraClick(number * clickPower){  //primary click function
+function liraClick(number){  //primary click function - add clickpower back if not causing glitch
     liras = liras + number;
     document.getElementById("liras").textContent = liras;  //use textContent instead of innerHTML
 };
@@ -20,8 +17,8 @@ function hireDeacon(){
     };
     var nextCost = Math.floor(100 * Math.pow(1.025,deacons));  //works out the cost of the next deacon
     document.getElementById('deaconCost').textContent = nextCost;  //updates the cost 
-    diaconalAchieved = 1;  //achievment
-	document.getElementById('diaconal').textContent = 'Orders I: Achieved!';
+    //diaconalAchieved = 1;  //achievment
+	//document.getElementById('diaconal').textContent = 'Orders I: Achieved!';
 };
 
 var priests = 0;
@@ -35,23 +32,11 @@ function hirePriest(){
     };
     var nextCost = Math.floor(1000 * Math.pow(1.025,priests));  
     document.getElementById('priestCost').textContent = nextCost;  
-    ordinationAchieved = 1;  
-	document.getElementById('ordination').textContent = 'Orders II: Achieved!';
+    //ordinationAchieved = 1;  
+	//document.getElementById('ordination').textContent = 'Orders II: Achieved!';
 };
 
 
-/*var pontificalKnighthood = 0;
-function pontificalKnighthood(){
-	var pontificalKnighthoodCost = 1000;
-	if(liras >= pontificalKnighthoodCost({
-		pontificalKnighthood = 1;
-		liras = liras - pontificalKnighthoodCost;
-		clickPower = 2;
-		document.getElementById('pontificalKnighthood').textContent = "Bought!"; 
-        document.getElementById('liras').textContent = liras;  
-	}
-};
-*/
 window.setInterval(function(){  //have buildings "click" every second
 	liraClick(deacons);
 	liraClick(priests * 10);
