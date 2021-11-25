63e6deffdfc1384d8b777e53b47c8d9f3fb65d84
XP-Studios <66804726+XP-Studios@users.noreply.github.com>
2021-11-25 15:09:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-var clickPower = 1; //liras per click
+var clickPower = 1; //wafers per click
 
-var liras = 0;
-function liraClick(number){  //primary click function - add clickpower back if not causing glitch
-    liras = liras + number;
-    document.getElementById("liras").textContent = liras;  //use textContent instead of innerHTML
+var wafers = 0;
+function waferClick(number){  //primary click function - add clickpower back if not causing glitch
+    wafers = wafers + number;
+    document.getElementById("wafers").textContent = wafers;  //use textContent instead of innerHTML
 };
 
 var deacons = 0;
 function hireDeacon(){
     var deaconCost = Math.floor(100 * Math.pow(1.025,deacons));     //works out cost 
-    if(liras >= deaconCost){                                   //checks that the player can afford item
+    if(wafers >= deaconCost){                                   //checks that the player can afford item
 		deacons = deacons + 1;
-        liras = liras - deaconCost;                          //removes spent liras
+        wafers = wafers - deaconCost;                          //removes spent wafers
         document.getElementById('deacons').textContent = deacons;  //updates the number of deacons 
-        document.getElementById('liras').textContent = liras;  //updates the number of liras
+        document.getElementById('wafers').textContent = wafers;  //updates the number of wafers
     };
     var nextCost = Math.floor(100 * Math.pow(1.025,deacons));  //works out the cost of the next deacon
     document.getElementById('deaconCost').textContent = nextCost;  //updates the cost 
-    //diaconalAchieved = 1;  //achievment
-	//document.getElementById('diaconal').textContent = 'Orders I: Achieved!';
+    
 };
 
 var priests = 0;
 function hirePriest(){
     var priestCost = Math.floor(1000 * Math.pow(1.025,priests));     
-    if(liras >= priestCost){                                   
+    if(wafers >= priestCost){                                   
 		priests = priests + 1;
-        liras = liras - priestCost;                       
+        wafers = wafers - priestCost;                       
         document.getElementById('priests').textContent = priests; 
-        document.getElementById('liras').textContent = liras;  
+        document.getElementById('wafers').textContent = wafers;  
     };
     var nextCost = Math.floor(1000 * Math.pow(1.025,priests));  
     document.getElementById('priestCost').textContent = nextCost;  
-    //ordinationAchieved = 1;  
-	//document.getElementById('ordination').textContent = 'Orders II: Achieved!';
+   
 };
 
-
-window.setInterval(function(){  //have buildings "click" every second
-	liraClick(deacons);
-	liraClick(priests * 10);
+window.setInterval(function(){  //lets deacons click once per second
+	waferClick(deacons);
 }, 1000);
+
+window.setInterval(function(){  //lets everything else click at rate of 0.1 seconds
+	waferClick(priests);
+}, 100);
