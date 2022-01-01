3a37ff4c965b354209c4abab8e34c4408f0b4be6
XP-Studios <66804726+XP-Studios@users.noreply.github.com>
2022-01-01 15:28:01 -0500

    0.0.4
    you now start with one deacon, CSS, introduced upgrade

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var clickPower = 1; //wafers per click
+var clickPower = 1; //wafers per click (works)
 
 var wafers = 0;
-function waferClick(number){  //primary click function - add clickpower back if not causing glitch
+function waferClick(number){  //primary click function 
     wafers = wafers + number;
     document.getElementById("wafers").textContent = wafers;  //use textContent instead of innerHTML
 };
 
-var deacons = 0;
+var deacons = 1;
 function hireDeacon(){
     var deaconCost = Math.floor(100 * Math.pow(1.025,deacons));     //works out cost 
     if(wafers >= deaconCost){                                   //checks that the player can afford item
@@ -34,10 +34,42 @@ function hirePriest(){
    
 };
 
+var monsignori = 0;
+function hireMonsignor(){
+	var monsignorCost = Math.floor(10000 * Math.pow(1.025,monsignori));  
+	if(wafers >= monsignorCost){   
+		monsignori = monsignori + 1;
+		wafers = wafers - monsignorCost;
+		document.getElementById('monsignori').textcontent = monsignori;
+		document.getElementById('wafers').textContent = wafers;  
+	}; 
+	var nextCost = Math.floor(10000 * Math.pow(1.025,monsignori));  
+    document.getElementById('monsignorCost').textContent = nextCost;  
+};
+
+//upgrades below here
+
+var clickUpgrade1 = false;
+function buyClickUpgrade1(){
+	if(wafers >= 2000 && clickUpgrade1 == false){
+		clickUpgrade1 = true;
+		wafers = wafers - 2000;
+		clickPower = clickPower * 2;
+		document.getElementById('clickUpgrade1Purchased').textContent = 'Yes!';
+		document.getElementById('wafers').textContent = wafers;
+	};
+};
+
+		
+		
+
+//click intervals below here
+
 window.setInterval(function(){  //lets deacons click once per second
 	waferClick(deacons);
 }, 1000);
 
 window.setInterval(function(){  //lets everything else click at rate of 0.1 seconds
 	waferClick(priests);
+	waferClick(monsignori * 10);
 }, 100);
