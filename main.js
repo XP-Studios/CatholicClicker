1ca5cb891aca51c3dbd2f37e23e6c21a1333f06d
XP-Studios <66804726+XP-Studios@users.noreply.github.com>
2022-01-04 15:05:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-var clickPower = 1; //wafers per click 
+var clickPower = 1; //wafers per click (works)
 
 var wafers = 0;
 function waferClick(number){  //primary click function 
     wafers = wafers + number;
     document.getElementById("wafers").textContent = wafers;  //use textContent instead of innerHTML
 };
+
+//hire clergy
 
 var deacons = 1;
 function hireDeacon(){
@@ -31,21 +33,85 @@ function hirePriest(){
     };
     var nextCost = Math.floor(1000 * Math.pow(1.025,priests));  
     document.getElementById('priestCost').textContent = nextCost;  
-   
+};
+
+var pastors = 0;
+function hirePastor(){
+    var pastorCost = Math.floor(10000 * Math.pow(1.025,pastors));     
+    if(wafers >= pastorCost){                                   
+		pastors = pastors + 1;
+        wafers = wafers - pastorCost;                       
+        document.getElementById('pastors').textContent = pastors; 
+        document.getElementById('wafers').textContent = wafers;  
+    };
+    var nextCost = Math.floor(1000 * Math.pow(1.025,priests));  
+    document.getElementById('priestCost').textContent = nextCost;  
 };
 
 var monsignori = 0;
 function hireMonsignor(){
-	var monsignorCost = Math.floor(10000 * Math.pow(1.025,monsignori));  
+	var monsignorCost = Math.floor(100000 * Math.pow(1.025,monsignori));  
 	if(wafers >= monsignorCost){   
 		monsignori = monsignori + 1;
 		wafers = wafers - monsignorCost;
-		document.getElementById('monsignori').textcontent = monsignori;
+		document.getElementById('monsignori').textContent = monsignori;
 		document.getElementById('wafers').textContent = wafers;  
 	}; 
-	var nextCost = Math.floor(10000 * Math.pow(1.025,monsignori));  
+	var nextCost = Math.floor(100000 * Math.pow(1.025,monsignori));  
     document.getElementById('monsignorCost').textContent = nextCost;  
 };
+
+//fire clergy
+
+function fireDeacon(){
+    var deaconCost = Math.floor(100 * Math.pow(1.025,deacons));     //works out cost 
+    if(deacons > 0){                                   //checks that the player can delete item
+		deacons = deacons - 1;                         
+        wafers = wafers + Math.round(.5 * deaconCost);                          //adds wafers
+        document.getElementById('deacons').textContent = deacons;  //updates the number of deacons 
+        document.getElementById('wafers').textContent = wafers;  //updates the number of wafers
+    };
+    var nextCost = Math.floor(100 * Math.pow(1.025,deacons));  //works out the cost of the next deacon
+    document.getElementById('deaconCost').textContent = nextCost;  //updates the cost 
+    
+};
+
+function firePriest(){
+    var priestCost = Math.floor(100 * Math.pow(1.025,priests));
+    if(priests > 0){                               
+		priests = priests - 1;
+        wafers = wafers + Math.round(.5 * priestCost);                      
+        document.getElementById('priests').textContent = priests;  
+        document.getElementById('wafers').textContent = wafers;  
+    };
+    var nextCost = Math.floor(100 * Math.pow(1.025,priests));  
+    document.getElementById('priestCost').textContent = nextCost;  
+};
+
+function firePastor(){
+    var pastorCost = Math.floor(100 * Math.pow(1.025,pastors));
+    if(pastors > 0){                               
+		pastors = pastors - 1;
+        wafers = wafers + Math.round(.5 * pastorCost);                      
+        document.getElementById('pastors').textContent = pastors;  
+        document.getElementById('wafers').textContent = wafers;  
+    };
+    var nextCost = Math.floor(100 * Math.pow(1.025,pastors));  
+    document.getElementById('pastorCost').textContent = nextCost;  
+};
+
+function fireMonsignor(){
+    var monsignorCost = Math.floor(100 * Math.pow(1.025,monsignori));
+    if(monsignori > 0){                               
+		monsignori = monsignori - 1;
+        wafers = wafers + Math.round(.5 * monsignori);                      
+        document.getElementById('priests').textContent = monsignori;  
+        document.getElementById('wafers').textContent = wafers;  
+    };
+    var nextCost = Math.floor(100 * Math.pow(1.025,monsignori));  
+    document.getElementById('priestCost').textContent = nextCost;  
+};
+
 
 //upgrades below here
 
@@ -59,10 +125,7 @@ function buyClickUpgrade1(){
 		document.getElementById('wafers').textContent = wafers;
 	};
 };
-
 		
-		
-
 //click intervals below here
 
 window.setInterval(function(){  //lets deacons click once per second
@@ -71,5 +134,6 @@ window.setInterval(function(){  //lets deacons click once per second
 
 window.setInterval(function(){  //lets everything else click at rate of 0.1 seconds
 	waferClick(priests);
-	waferClick(monsignori * 10);
+	waferClick(pastors * 10);
+	waferClick(monsignori * 100);
 }, 100);
