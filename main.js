3bc749582c5fd2c6243dbeac73981d3df81f7104
XP-Studios <66804726+XP-Studios@users.noreply.github.com>
2022-01-23 09:11:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -61,9 +61,9 @@ function hireMonsignor(){
     document.getElementById('monsignorCost').textContent = nextCost;  
 };
 
-//fire clergy
+//laicize clergy
 
-function fireDeacon(){
+function laicizeDeacon(){
     var deaconCost = Math.floor(100 * Math.pow(1.025,deacons));     //works out cost 
     if(deacons > 0){                                   //checks that the player can delete item
 		deacons = deacons - 1;                         
@@ -76,7 +76,7 @@ function fireDeacon(){
     
 };
 
-function firePriest(){
+function laicizePriest(){
     var priestCost = Math.floor(100 * Math.pow(1.025,priests));
     if(priests > 0){                               
 		priests = priests - 1;
@@ -88,7 +88,7 @@ function firePriest(){
     document.getElementById('priestCost').textContent = nextCost;  
 };
 
-function firePastor(){
+function laicizePastor(){
     var pastorCost = Math.floor(100 * Math.pow(1.025,pastors));
     if(pastors > 0){                               
 		pastors = pastors - 1;
@@ -100,7 +100,7 @@ function firePastor(){
     document.getElementById('pastorCost').textContent = nextCost;  
 };
 
-function fireMonsignor(){
+function laicizeMonsignor(){
     var monsignorCost = Math.floor(100 * Math.pow(1.025,monsignori));
     if(monsignori > 0){                               
 		monsignori = monsignori - 1;
@@ -125,7 +125,38 @@ function buyClickUpgrade1(){
 		document.getElementById('wafers').textContent = wafers;
 	};
 };
-		
+
+//mouseover events for clergy below here
+function deaconMouseover(){
+	document.getElementById('deaconInfo').textContent = 'A cleric whose job is to assist the priest at Mass, particularly by proclaiming the Gospel. They may be married in both Western and Eastern Catholicism.'
+
+}
+function deaconMouseout(){
+	document.getElementById('deaconInfo').textContent = ''
+}
+
+function priestMouseover(){
+	document.getElementById('priestInfo').textContent = 'The most common type of cleric. He says Mass, including the homily (sermon), and administers the other sacraments, except ordination and confirmation (ministered by bishop) and marriage (ministered by the couple). They may be married in Eastern Catholicism but generally cannot be in the West.'
+
+}
+function priestMouseout(){
+	document.getElementById('priestInfo').textContent = ''
+}
+	
+function pastorMouseover(){
+	document.getElementById('pastorInfo').textContent = 'Technically not a rank of cleric: a priest who is the head of a parish (individual church).'
+}
+function pastorMouseout(){
+	document.getElementById('pastorInfo').textContent = ''
+}
+
+function monsignorMouseover(){
+	document.getElementById('monsignorInfo').textContent = 'Technically not a rank of cleric: a title of honor signifying that the priest is either a Protonotary Apostolic, Honorary Prelate, or Chaplain of His Holiness. Given to Vicars-General and any priest who the Pope gives the honor.'
+}
+function monsignorMouseout(){
+	document.getElementById('monsignorInfo').textContent = ''
+}
+	
 //click intervals below here
 
 window.setInterval(function(){  //lets deacons click once per second
